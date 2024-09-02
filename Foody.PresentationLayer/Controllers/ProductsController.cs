30dd430fb416625d3833e922b55e1d1064e1a38f
Tolunay <88236341+TolunayYilmaz@users.noreply.github.com>
2024-09-02 12:16:29 +0300

    Added DtoLayer Classes

--- Foody.PresentationLayer/Controllers/ProductsController.cs
+++ Foody.PresentationLayer/Controllers/ProductsController.cs
@@ -1,15 +1,18 @@
 ﻿using Foody.BusinessLayer.Abstract;
+using Foody.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Foody.PresentationLayer.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
-
-        public ProductsController(IProductService productService)
+        private readonly ICategoryService _categoryService;
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         public IActionResult ProductList()
@@ -29,8 +32,32 @@ namespace Foody.PresentationLayer.Controllers
         }
         [HttpGet]
         public IActionResult CreateProduct()
-        { 
+        {
+            var values = _categoryService.TGetAll();
+            ViewBag.categories = new SelectList(values,"CategoryId","CategoryName");
             return View ();
+        }
+        
+        [HttpPost]
+        public IActionResult CreateProduct(Product  product)
+        {
+            _productService.TInsert(product);
+            return RedirectToAction("ProductListWithCategory");
+        }
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var values = _categoryService.TGetAll();
+            ViewBag.categories = new SelectList(values, "CategoryId", "CategoryName");
+            var productValues =_productService.TGetById(id);
+
+            return View(productValues);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            _productService.TUpdate(product);
+            return RedirectToAction("ProductListWithCategory");
         }
     }
 }
