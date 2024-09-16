60f5be6ac182c486e7eac00e9411f82d24db2471
Tolunay <88236341+TolunayYilmaz@users.noreply.github.com>
2024-09-16 12:35:34 +0300

    Added Slider Operations

--- Foody.EntityLayer/Concrete/Feature.cs
+++ Foody.EntityLayer/Concrete/Feature.cs
@@ -12,5 +12,6 @@ namespace Foody.EntityLayer.Concrete
         public string Title { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
+ 
     }
 }
